52cea996ce232521fe3f5732e88fcbb8e779c84c
Wenderson <wps713@gmail.com>
2020-03-22 17:43:52 -0300

    Add signin page content & style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import logo from '~/assets/fastfeet-logo.png';
+import { Container, Form } from './styles';
 
 export default function SignIn() {
   return (
-    <div>
-      <h3>SignIn</h3>
-    </div>
+    <Container>
+      <img src={logo} alt="FastFeet" />
+      <Form>
+        <label htmlFor="email">Seu e-mail</label>
+        <input type="email" id="email" placeholder="exemplo@email.com" />
+
+        <label htmlFor="password">Sua senha</label>
+        <input type="password" id="password" placeholder="*************" />
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </Container>
   );
 }
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  border-radius: 4px;
+  img {
+    margin: 40px 20px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  button {
+    background-color: #7d40e7;
+    margin-bottom: 20px;
+  }
+`;
