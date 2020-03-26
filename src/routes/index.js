5f18259a25851abbcfde10fd5b4a7bfcc1aa5d89
Wenderson <wps713@gmail.com>
2020-03-26 20:17:47 -0300

    Add deliveries route and component

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import Dashboard from '~/pages/Dashboard';
+import Deliveries from '~/pages/Deliveries';
 import SignIn from '~/pages/SignIn';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" exact component={Dashboard} isPrivate />
+      <Route path="/deliveries" exact component={Deliveries} isPrivate />
     </Switch>
   );
 }
