0a7e998477c0804bae263eb1f3382ab5d6d3e72b
Wenderson <wps713@gmail.com>
2020-03-27 10:06:04 -0300

    Add deliveries page style

--- src/pages/Deliveries/styles.js
+++ src/pages/Deliveries/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin: 0% 10% auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Table = styled.table``;
 
