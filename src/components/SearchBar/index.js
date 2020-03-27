aae0b8feebe75da134e29dc4de53405b6db246ac
Wenderson <wps713@gmail.com>
2020-03-27 18:34:51 -0300

    Add search bard content

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Container, Input } from './styles';
 
-// import { Container } from './styles';
-
-export default function SearchBar() {
-  return <div />;
+export default function SearchBar({ placeholder = 'Faça sua busca' }) {
+  return (
+    <Container>
+      <Input type="text" placeholder={placeholder} />
+    </Container>
+  );
 }
---
+++ src/components/SearchBar/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 20%;
+`;
+
+export const Input = styled.input`
+  padding: 8px;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+`;
