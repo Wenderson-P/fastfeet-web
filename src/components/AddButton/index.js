0254a0ef0a022a89ea8e60d368d9db7e2c8b6019
Wenderson <wps713@gmail.com>
2020-03-27 16:57:42 -0300

    Add add Buttoncomponent

---
+++ src/components/AddButton/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function AddButton() {
+  return (
+    <Container>
+      <button type="button">Cadastrar</button>
+    </Container>
+  );
+}
---
+++ src/components/AddButton/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  button {
+    background-color: #7d40e7;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    padding: 10px;
+  }
+`;
