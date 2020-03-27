d8ac111742aaa3a0051826876b17368b024e097a
Wenderson <wps713@gmail.com>
2020-03-27 17:18:22 -0300

    Add mdAdd icon

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { MdAdd } from 'react-icons/md';
 
 import { Container } from './styles';
 
 export default function AddButton() {
   return (
     <Container>
-      <button type="button">Cadastrar</button>
+      <button type="button">
+        <MdAdd size={16} />
+        Cadastrar
+      </button>
     </Container>
   );
 }
