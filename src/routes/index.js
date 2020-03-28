887b4ae56d6b885dbf932c2ac18c410562917d29
Wenderson <wps713@gmail.com>
2020-03-28 11:30:20 -0300

    Add route to deliveries/add-delivery

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Deliveries from '~/pages/Deliveries';
+import AddDelivery from '~/pages/Deliveries/AddDelivery';
 import SignIn from '~/pages/SignIn';
 import DeliveryMen from '~/pages/DeliveryMen';
 import Recipient from '~/pages/Recipient';
@@ -12,7 +13,15 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+
       <Route path="/deliveries" exact component={Deliveries} isPrivate />
+      <Route
+        path="/deliveries/add-delivery"
+        exact
+        component={AddDelivery}
+        isPrivate
+      />
+
       <Route path="/deliverymen" exact component={DeliveryMen} isPrivate />
       <Route path="/recipient" exact component={Recipient} isPrivate />
       <Route path="/problems" exact component={Problems} isPrivate />
