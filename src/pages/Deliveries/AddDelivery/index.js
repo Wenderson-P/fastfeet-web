d71b4e5e592c7dd293091a34c75d210ddab0d7bd
Wenderson <wps713@gmail.com>
2020-03-28 14:51:56 -0300

    Changing form & sending data to selected

--- src/pages/Deliveries/AddDelivery/index.js
+++ src/pages/Deliveries/AddDelivery/index.js
@@ -1,29 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Yup from 'yup';
 
-import { Form, Input } from '@rocketseat/unform';
-import { Container, Header, Buttons, Row, Select, Item } from './styles';
+import {
+  Container,
+  Header,
+  Buttons,
+  Form,
+  Row,
+  Input,
+  Select,
+  Item,
+} from './styles';
+
+import api from '~/services/api';
+import history from '~/services/history';
 
 import SaveButton from '~/components/SaveButton';
 import GoBackButton from '~/components/GoBackButton';
 import SelectInput from '~/components/SelectInput';
 
 export default function AddDelivery() {
+  const [deliverymen, setDeliverymen] = useState([]);
+
+  const [selectedDeliveryman, setselectedDeliveryman] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState([]);
+
+  const [recipients, setRecipients] = useState([]);
+  const [selectedRecipient, setSelectedRecipient] = useState([]);
+
+  useEffect(() => {
+    async function loadDeliveryMen() {
+      const response = await api.get('/deliveryman');
+
+      const data = response.data.map(deliveryman => ({
+        value: deliveryman.id,
+        label: deliveryman.name,
+      }));
+      setDeliverymen(data);
+    }
+
+    async function loadRecipient() {
+      const response = await api.get('/recipients');
+
+      const data = response.data.map(recipient => ({
+        value: recipient.id,
+        label: recipient.name,
+      }));
+
+      setRecipients(data);
+    }
+    loadDeliveryMen();
+    loadRecipient();
+  }, []);
+
   return (
     <Container>
       <Header>
         <h2>Cadastro de encomendas</h2>
         <Buttons>
           <GoBackButton />
-          <SaveButton />
+          <SaveButton onClick={handleSubmit} />
         </Buttons>
       </Header>
       <Form>
         <Row>
           <Select>
-            <SelectInput />
+            <span>Destinatário</span>
+            <SelectInput
+              options={recipients}
+              valueSelected={setSelectedRecipient}
+            />
           </Select>
           <Select>
-            <SelectInput />
+            <span>Entregador</span>
+            <SelectInput
+              options={deliverymen}
+              valueSelected={setselectedDeliveryman}
+            />
           </Select>
         </Row>
         <Row>
@@ -32,6 +85,7 @@ export default function AddDelivery() {
               name="product"
               label="Produto"
               placeholder="Digite o nome do produto"
+              onChange={e => setSelectedProduct(e.target.value)}
             />
           </Item>
         </Row>
--- src/pages/Deliveries/AddDelivery/styles.js
+++ src/pages/Deliveries/AddDelivery/styles.js
@@ -4,21 +4,22 @@ export const Container = styled.div`
   margin: 0% 10% auto;
   display: flex;
   flex-direction: column;
-
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-
-    background-color: #fff;
-    border-radius: 4px;
-    padding: 25px 20px;
-  }
 `;
 
 export const Header = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 30px;
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 25px 20px;
 `;
 
 export const Buttons = styled.div`
@@ -33,10 +34,31 @@ export const Row = styled.div`
   align-self: stretch;
   margin-bottom: 16px;
 `;
+
+export const Input = styled.input`
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+
+  font-size: 16px;
+  color: #999999;
+  padding: 12px 15px;
+
+  transition: 0.5s;
+
+  &:hover {
+    border-color: #7d40e7;
+  }
+`;
 export const Select = styled.div`
   width: 100%;
+
   &:first-child {
     margin-right: 20px;
+  }
+
+  span {
+    font-weight: bold;
+    color: #444444;
   }
 `;
 export const Item = styled.div`
@@ -48,13 +70,5 @@ export const Item = styled.div`
     font-weight: bold;
     margin-bottom: 5px;
     color: #444444;
-  }
-  input {
-    border: 1px solid #dddddd;
-    border-radius: 4px;
-
-    font-size: 16px;
-    color: #999999;
-    padding: 12px 15px;
   }
 `;
