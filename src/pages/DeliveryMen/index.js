4c5cf1c02016262e4fe25ce0cedd14fab5a784ef
Wenderson <wps713@gmail.com>
2020-03-28 18:42:14 -0300

    Add deliverymen page content & style

--- src/pages/DeliveryMen/index.js
+++ src/pages/DeliveryMen/index.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Container, Table, Deliveryman, Avatar, Division } from './styles';
 
-export default function DeliveryMen() {
-  return <div />;
+import api from '~/services/api';
+
+import SearchBar from '~/components/SearchBar';
+import AddButton from '~/components/AddButton';
+import ActionMenu from '~/components/ActionMenu';
+
+export default function AddDeliveryMan() {
+  const [deliverymen, setDeliverymen] = useState([]);
+
+  useEffect(() => {
+    async function loadDeliveryMen() {
+      const response = await api.get('deliveryman');
+
+      setDeliverymen(response.data);
+    }
+    loadDeliveryMen();
+  }, []);
+
+  return (
+    <Container>
+      <h2>Gerenciando entregadores</h2>
+      <Division>
+        <SearchBar placeholder="Buscar por entregadores" />
+        <AddButton url="/deliverymen/add-deliveryman" />
+      </Division>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Foto</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deliverymen.map(deliveryman => (
+            <tr>
+              <td>#{deliveryman.id}</td>
+              <Deliveryman>
+                <Avatar
+                  src={
+                    deliveryman.avatar
+                      ? deliveryman.avatar.url
+                      : `https://ui-avatars.com/api/?name=${deliveryman.name}&background=8171E7&color=fff`
+                  }
+                  alt="Foto do entregador"
+                />
+              </Deliveryman>
+              <td>{deliveryman.name}</td>
+              <td>{deliveryman.email}</td>
+              <td>
+                <ActionMenu />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
---
+++ src/pages/DeliveryMen/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0% 10% auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Table = styled.table``;
+
+export const Deliveryman = styled.td`
+  display: flex;
+  align-items: center;
+`;
+
+export const Avatar = styled.img`
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+`;
+
+export const Division = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+`;
