23381613b8a1edd452e24f508a0b2cfa1f60a57c
Wenderson <wps713@gmail.com>
2020-03-28 18:43:29 -0300

    Add route to addDeliveryMan

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
+import SignIn from '~/pages/SignIn';
+
 import Deliveries from '~/pages/Deliveries';
 import AddDelivery from '~/pages/Deliveries/AddDelivery';
-import SignIn from '~/pages/SignIn';
+
 import DeliveryMen from '~/pages/DeliveryMen';
+import AddDeliveryMan from '~/pages/DeliveryMen/AddDeliveryMan';
+
 import Recipient from '~/pages/Recipient';
 import Problems from '~/pages/Problems';
 
@@ -23,6 +27,13 @@ export default function Routes() {
       />
 
       <Route path="/deliverymen" exact component={DeliveryMen} isPrivate />
+      <Route
+        path="/deliverymen/add-deliveryman"
+        exact
+        component={AddDeliveryMan}
+        isPrivate
+      />
+
       <Route path="/recipient" exact component={Recipient} isPrivate />
       <Route path="/problems" exact component={Problems} isPrivate />
       <Route path="" exact component={Deliveries} isPrivate />
