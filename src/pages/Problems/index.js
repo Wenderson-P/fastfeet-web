80e59a13fa10db54cd2a458a00c7b2a0196945e0
Wenderson <wps713@gmail.com>
2020-03-28 18:42:27 -0300

    Add problems page content & style

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Container, Table } from './styles';
+
+import api from '~/services/api';
+
+import ActionMenu from '~/components/ActionMenu';
 
 export default function Problems() {
-  return <div />;
+  const [problems, setProblems] = useState([]);
+
+  useEffect(() => {
+    async function loadProblems() {
+      const response = await api.get('delivery/problems');
+
+      setProblems(response.data);
+    }
+    loadProblems();
+  }, []);
+
+  return (
+    <Container>
+      <h2>Gerenciando Problemas</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Encomenda</th>
+            <th>Problema</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {problems.map(problem => (
+            <tr>
+              <td>#{problem.id}</td>
+              <td>{problem.description}</td>
+              <td>
+                <ActionMenu />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
---
+++ src/pages/Problems/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0% 10% auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Table = styled.table``;
