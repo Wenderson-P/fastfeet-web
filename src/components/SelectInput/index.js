490b236e77410d1a832263fab3df64c11a38bbf8
Wenderson <wps713@gmail.com>
2020-03-28 14:52:16 -0300

    Add options & on change in select input

--- src/components/SelectInput/index.js
+++ src/components/SelectInput/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
 
-export default function SelectInput() {
-  return <Select />;
+export default function SelectInput({ options, valueSelected }) {
+  return <Select options={options} onChange={value => valueSelected(value)} />;
 }
