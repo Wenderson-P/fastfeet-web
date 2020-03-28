608bf1a6c3bb0b699159061e9230a94878ace704
Wenderson <wps713@gmail.com>
2020-03-28 18:42:37 -0300

    Add recipients page content & style

--- src/pages/Recipient/index.js
+++ src/pages/Recipient/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Container, Table, Division } from './styles';
 
-export default function Recipient() {
-  return <div />;
+import api from '~/services/api';
+
+import SearchBar from '~/components/SearchBar';
+import AddButton from '~/components/AddButton';
+import ActionMenu from '~/components/ActionMenu';
+
+export default function Deliveries() {
+  const [recipients, setRecipients] = useState([]);
+
+  useEffect(() => {
+    async function loadRecipients() {
+      const response = await api.get('recipients');
+
+      setRecipients(response.data);
+    }
+    loadRecipients();
+  }, []);
+
+  return (
+    <Container>
+      <h2>Gerenciando destinatários</h2>
+      <Division>
+        <SearchBar placeholder="Buscar por destinatários" />
+        <AddButton url="/deliverymen/add-recipient" />
+      </Division>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipients.map(recipient => (
+            <tr>
+              <td>#{recipient.id}</td>
+              <td>{recipient.name}</td>
+              <td>
+                {recipient.street}, {recipient.number}, {recipient.city} -{' '}
+                {recipient.state}
+              </td>
+              <td>
+                <ActionMenu />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
---
+++ src/pages/Recipient/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0% 10% auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Table = styled.table``;
+
+export const Division = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+`;
