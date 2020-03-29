94a1862cee416e07cfd1132e9bcb7e90e52fb5bb
Wenderson <wps713@gmail.com>
2020-03-29 11:07:41 -0300

    Showing content in action menu if props are true

--- src/components/ActionMenu/index.js
+++ src/components/ActionMenu/index.js
@@ -16,21 +16,31 @@ export default function ActionMenu({ visualize, edit, erase, eraseLabel }) {
         <MdMoreHoriz size={16} color="#C6C6C6" />
       </button>
       <Modal hide={hide}>
-        <button type="button">
-          <MdVisibility size={14} color="#8E5BE8" />
-          Visualizar
-        </button>
-        <hr />
-        <button type="button">
-          <MdCreate size={14} color="#4D85EE" />
-          Editar
-        </button>
-        <hr />
+        {visualize && (
+          <>
+            <button type="button">
+              <MdVisibility size={14} color="#8E5BE8" />
+              Visualizar
+            </button>
 
-        <button type="button">
-          <MdDeleteForever size={14} color="#DE3B3B" />
-          Excluir
-        </button>
+            <hr />
+          </>
+        )}
+        {edit && (
+          <>
+            <button type="button">
+              <MdCreate size={14} color="#4D85EE" />
+              Editar
+            </button>
+            <hr />
+          </>
+        )}
+        {erase && (
+          <button type="button">
+            <MdDeleteForever size={14} color="#DE3B3B" />
+            {eraseLabel || 'Excluir'}
+          </button>
+        )}
       </Modal>
     </Container>
   );
