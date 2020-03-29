3c0e940de0d3e39ee89f55ff4af50ae9db52740e
Wenderson <wps713@gmail.com>
2020-03-29 11:10:08 -0300

    Changing action menu width & left location

--- src/components/ActionMenu/styles.js
+++ src/components/ActionMenu/styles.js
@@ -6,18 +6,18 @@ export const Container = styled.div``;
 export const Modal = styled.div`
   display: ${props => (props.hide ? 'none' : 'block')};
   position: absolute;
-  width: 150px;
   background-color: #fff;
   padding: 15px 10px;
-  left: calc(90% - 180px);
+  left: calc(82%);
   box-shadow: 0px 0px 2px #00000026;
   border-radius: 4px;
 
   button {
+    width: 100%;
     font-size: 16px;
+    min-width: 120px;
+    text-align: left;
     color: #999999;
-    padding-right: 50px;
-    margin: 5px 0px;
     svg {
       margin-right: 10px;
     }
