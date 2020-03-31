4eac5eb412967651ebb83295f62d8992dadc6361
Wenderson <wps713@gmail.com>
2020-03-31 19:23:37 -0300

    add AvatarInput style

--- src/pages/DeliveryMen/AddDeliveryMan/AvatarInput/styles.js
+++ src/pages/DeliveryMen/AddDeliveryMan/AvatarInput/styles.js
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.label`
+  align-self: center;
+  margin-bottom: 25px;
+  display: flex;
+  flex-direction: column;
+  color: #dddddd;
+  align-items: center;
+  justify-content: center;
+  height: 150px;
+  width: 150px;
+  border: 1px dotted #dddddd;
+  border-radius: 50%;
+
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+
+  input {
+    display: none;
+  }
+`;
