d982575cc92c07ecb62a6f3ad7f5ba60c21ad112
Wenderson <wps713@gmail.com>
2020-04-02 14:42:27 -0300

    Add onchange action in searchbar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Input } from './styles';
 
-export default function SearchBar(
-  { placeholder = 'Faça sua busca' },
-  onChange
-) {
+export default function SearchBar({ placeholder = 'Faça sua busca', search }) {
   return (
     <Container>
-      <Input type="text" placeholder={placeholder} onChange={onChange} />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        onChange={e => search(e.target.value)}
+      />
     </Container>
   );
 }
