7c12ea4f2701b93ae6404441e07e8505b51452ed
Wenderson <wps713@gmail.com>
2020-04-03 11:27:36 -0300

    Add pagination limit from 1 to 15

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
 import { Container, Button, PageBlock } from './styles';
 
 export default function Pagination({ actualPage = 1, changePage }) {
-  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  useEffect(() => {
+    if (actualPage <= 0) {
+      changePage(1);
+    }
+    const lastpage = pages[pages.length - 1];
+
+    if (actualPage > lastpage) {
+      changePage(actualPage - 1);
+    }
+  }, [actualPage]);
+
   return (
     <Container>
       <Button onClick={() => changePage(actualPage - 1)}>
