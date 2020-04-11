0d909897aaa55dd9c8858ffaaf5031090bd05050
Wenderson <wps713@gmail.com>
2020-04-10 23:00:13 -0300

    Add sign failure & out reducer

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -13,6 +13,15 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.token = action.payload.token;
         draft.signed = true;
       });
+    case '@auth/SIGN_IN_FAILURE':
+      return produce(state, draft => {
+        draft.loading = false;
+      });
+    case '@auth/SIGN_OUT':
+      return produce(state, draft => {
+        draft.token = null;
+        draft.signed = false;
+      });
     default:
       return state;
   }
