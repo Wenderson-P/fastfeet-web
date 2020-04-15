70cf469cadd5bda12e5fdd6b287f64fbc2ea8fb4
Wenderson <wps713@gmail.com>
2020-04-15 19:08:09 -0300

    Changing searchBar Container to 'fit' icon
    Now the icon is inside the container

--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -2,10 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 20%;
+  height: 36px;
+
+  display: flex;
+  align-items: center;
+
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  svg {
+    padding-left: 8px;
+  }
 `;
 
 export const Input = styled.input`
   padding: 8px;
-  border: 1px solid #dddddd;
+  border: none;
+  background: none;
   border-radius: 4px;
+  font-size: 14px;
+  border: 0;
 `;
