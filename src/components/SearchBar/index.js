92309e6b3d5b69b24861d9df758303d95bb9af0d
Wenderson <wps713@gmail.com>
2020-04-15 19:08:01 -0300

    Add Search Icon in SeachBar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 import { Container, Input } from './styles';
 
 export default function SearchBar({ placeholder = 'Faça sua busca', search }) {
   return (
     <Container>
+      <MdSearch color="#999" size={16} />
       <Input
         type="text"
         placeholder={placeholder}
